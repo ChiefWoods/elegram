b04797d353fbd4893fb39539c8829ada4b63af03
Chii Yuen <chii.yuen@hotmail.com>
2026-05-14 01:19:42 +0800

    Add root route

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,9 +1,10 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+import { getSession } from "@/lib/auth-client";
 
 export const Route = createFileRoute("/")({
-  component: RouteComponent,
+  beforeLoad: async () => {
+    const { data } = await getSession();
+    throw redirect({ to: data ? "/chat" : "/login" });
+  },
 });
-
-function RouteComponent() {
-  return <div>Hello "/"!</div>;
-}
